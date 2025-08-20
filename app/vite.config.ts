7d699123a3e7fbf795746c120578bd5af12d61c4
Øyvind Nygard <oyvinyg@gmail.com>
2025-08-20 11:05:50 +0200

    Server react-applikasjonen (#5)

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,20 @@
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+import { fileURLToPath, URL } from "node:url";
+
+import { tanstackRouter } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [react(), tanstackRouter()],
+  resolve: {
+    alias: {
+      "~": fileURLToPath(new URL("src", import.meta.url)),
+    },
+  },
+  server: {
+    cors: {
+      origin: [new RegExp("dev.nav.no$")],
+    },
+    origin: "http://localhost:5173",
+  },
 });
