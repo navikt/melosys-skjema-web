844aa36b90361629ba01eee307ab430ba32a9fa3
oyvinyg <oyvinyg@gmail.com>
2025-08-20 20:10:55 +0200

    Starte wildcard path med /

--- server/src/frontendRoute.ts
+++ server/src/frontendRoute.ts
@@ -61,7 +61,7 @@ export function setupStaticRoutes(router: Router) {
   const spaFilePath = path.resolve("./public", "index.html");
 
   // Fra Express 5 er wildcard ruten erstattet med *splat: https://expressjs.com/en/guide/migrating-5.html
-  router.get("*splat", async (request, response) => {
+  router.get("/*splat", async (request, response) => {
     console.log("Henter dekorator");
 
     const html = await injectDecoratorServerSide({
