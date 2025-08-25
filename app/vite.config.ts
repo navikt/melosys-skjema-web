1c4b7967ca2b9d00a4801d7a1e91b9c603b848d6
oyvinyg <oyvinyg@gmail.com>
2025-08-25 13:36:11 +0200

    Lagt på litt layout

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,11 +1,12 @@
 import { fileURLToPath, URL } from "node:url";
 
+import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(), tanstackRouter()],
+  plugins: [react(), tanstackRouter(), tailwindcss()],
   resolve: {
     alias: {
       "~": fileURLToPath(new URL("src", import.meta.url)),
