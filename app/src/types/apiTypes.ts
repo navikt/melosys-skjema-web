53e0d78bbd5b7b157446949a986a0ff8b12d64d6
oyvinyg <oyvinyg@gmail.com>
2025-09-05 09:43:18 +0200

    Autogenerer typer fra melosys-skjema-api sin openapi doc

---
+++ app/src/types/apiTypes.ts
@@ -0,0 +1,14 @@
+/*
+ * ---------------------------------------------------------------
+ * ## THIS FILE WAS GENERATED VIA SWAGGER-TYPESCRIPT-API        ##
+ * ##                                                           ##
+ * ## AUTHOR: acacode                                           ##
+ * ## SOURCE: https://github.com/acacode/swagger-typescript-api ##
+ * ---------------------------------------------------------------
+ */
+
+export interface OrganisasjonDto {
+  orgnr: string;
+  navn: string;
+  organisasjonsform: string;
+}
