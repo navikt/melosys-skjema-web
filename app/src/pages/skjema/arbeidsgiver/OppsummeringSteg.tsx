caa263982f78e513d72b801604b63c9c7d1fdb97
oyvinyg <oyvinyg@gmail.com>
2025-09-26 12:45:32 +0200

    Legg til steg rekkefølge for OppsummeringSteg og VeiledningSteg

--- app/src/pages/skjema/arbeidsgiver/OppsummeringSteg.tsx
+++ app/src/pages/skjema/arbeidsgiver/OppsummeringSteg.tsx
@@ -2,6 +2,8 @@ import { PaperplaneIcon } from "@navikt/aksel-icons";
 
 import { SkjemaSteg } from "~/pages/skjema/components/SkjemaSteg.tsx";
 
+import { ARBEIDSGIVER_STEG_REKKEFOLGE } from "./stegRekkefølge.ts";
+
 const stepKey = "oppsummering";
 
 export function OppsummeringSteg() {
@@ -9,6 +11,7 @@ export function OppsummeringSteg() {
     <SkjemaSteg
       config={{
         stepKey,
+        stegRekkefolge: ARBEIDSGIVER_STEG_REKKEFOLGE,
         customNesteKnapp: {
           tekst: "Send søknad",
           ikon: <PaperplaneIcon />,
