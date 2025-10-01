287651fdf9cd18e93dc873e7b0ba72d2de1dcc65
oyvinyg <oyvinyg@gmail.com>
2025-10-01 11:52:56 +0200

    Implementert oppsett for flerspråklig støtte med i18n og nav-dekorator

---
+++ app/src/i18n/i18n.ts
@@ -0,0 +1,144 @@
+export const resources = {
+  nb: {
+    translation: {
+      felles: {
+        laster: "Laster...",
+        feil: "Det oppstod en feil",
+        lagre: "Lagre",
+        lagreOgFortsett: "Lagre og fortsett",
+        sendSoknad: "Send søknad",
+        avbryt: "Avbryt",
+        ja: "Ja",
+        nei: "Nei",
+        tilbake: "Tilbake",
+        neste: "Neste",
+        fjern: "Fjern",
+        leggTil: "Legg til",
+        pakrevd: "påkrevd",
+      },
+      rollevelger: {
+        tittel: "Hvem vil du fylle ut skjema på vegne av?",
+        degSelv: "Deg selv",
+        orgNummer: "Org.nr.",
+      },
+      arbeidstaker: {
+        harNorskFodselsnummer:
+          "Har arbeidstakeren norsk fødselsnummer eller d-nummer?",
+        fodselsnummerEllerDNummer:
+          "Arbeidstakerens fødselsnummer eller d-nummer",
+        fornavn: "Arbeidstakerens fornavn",
+        etternavn: "Arbeidstakerens etternavn",
+        fodselsdato: "Arbeidstakerens fødselsdato",
+        harVaertEllerSkalVaereILonnetArbeid:
+          "Har du vært eller skal du være i lønnet arbeid i Norge i minst én måned rett før utsendingen?",
+        aktivitet: "Aktivitet (TODO: finne en bra label her)",
+        velgAktivitet: "Velg aktivitet",
+        skalJobbeForFlereVirksomheter:
+          "Skal du jobbe for flere virksomheter i perioden du søker for?",
+      },
+      validering: {
+        pakrevd: "er påkrevd",
+        organisasjonsnummerPakrevd: "Organisasjonsnummer er påkrevd",
+        organisasjonsnummerFormat: "Organisasjonsnummer må være 9 siffer",
+        virksomhetsnavnPakrevd: "Navn på virksomhet er påkrevd",
+        adressePakrevd: "Vegnavn og husnummer er påkrevd",
+        landPakrevd: "Land er påkrevd",
+        maSvarePaSammeKonsern:
+          "Du må svare på om virksomheten tilhører samme konsern",
+        maSvarePaNorskFodselsnummer:
+          "Du må svare på om arbeidstakeren har norsk fødselsnummer",
+        fodselsnummerPakrevdNarNorsk:
+          "Fødselsnummer eller d-nummer er påkrevd når arbeidstakeren har norsk fødselsnummer",
+        fodselsnummerMaVare11Siffer:
+          "Fødselsnummer eller d-nummer må være 11 siffer",
+        fornavnPakrevd: "Fornavn er påkrevd og må være minst 2 tegn",
+        etternavnPakrevd: "Etternavn er påkrevd og må være minst 2 tegn",
+        fodselsdatoPakrevd: "Fødselsdato er påkrevd",
+        maVelgeAktivitetNarIkkeLonnetArbeid:
+          "Du må velge en aktivitet når du ikke har vært i lønnet arbeid",
+        maSvarePaLonnetArbeid:
+          "Du må svare på om du har vært eller skal være i lønnet arbeid i Norge før utsending",
+        maSvarePaFlereVirksomheter:
+          "Du må svare på om du skal jobbe for flere virksomheter i perioden",
+      },
+      aktiviteter: {
+        studier: "Studier",
+        ferie: "Ferie",
+        selvstendingVirksomhet: "Selvstendig virksomhet",
+        kontantytelseFraNav: "Mottok kontantytelse fra Nav",
+      },
+    },
+  },
+  en: {
+    translation: {
+      felles: {
+        laster: "Loading...",
+        feil: "An error occurred",
+        lagre: "Save",
+        lagreOgFortsett: "Save and continue",
+        sendSoknad: "Submit application",
+        avbryt: "Cancel",
+        ja: "Yes",
+        nei: "No",
+        tilbake: "Back",
+        neste: "Next",
+        fjern: "Remove",
+        leggTil: "Add",
+        pakrevd: "required",
+      },
+      rollevelger: {
+        tittel: "Who do you want to fill out the form for?",
+        degSelv: "Yourself",
+        orgNummer: "Org. no.",
+      },
+      arbeidstaker: {
+        harNorskFodselsnummer:
+          "Does the employee have a Norwegian social security number or D-number?",
+        fodselsnummerEllerDNummer:
+          "Employee's social security number or D-number",
+        fornavn: "Employee's first name",
+        etternavn: "Employee's last name",
+        fodselsdato: "Employee's date of birth",
+        harVaertEllerSkalVaereILonnetArbeid:
+          "Have you been or will you be in paid employment in Norway for at least one month before the assignment?",
+        aktivitet: "Activity (TODO: find a good label here)",
+        velgAktivitet: "Select activity",
+        skalJobbeForFlereVirksomheter:
+          "Will you work for multiple companies during the period you are applying for?",
+      },
+      validering: {
+        pakrevd: "is required",
+        organisasjonsnummerPakrevd: "Organization number is required",
+        organisasjonsnummerFormat: "Organization number must be 9 digits",
+        virksomhetsnavnPakrevd: "Company name is required",
+        adressePakrevd: "Street address and house number is required",
+        landPakrevd: "Country is required",
+        maSvarePaSammeKonsern:
+          "You must answer whether the company belongs to the same consortium",
+        maSvarePaNorskFodselsnummer:
+          "You must answer whether the employee has a Norwegian social security number",
+        fodselsnummerPakrevdNarNorsk:
+          "Social security number or D-number is required when the employee has a Norwegian social security number",
+        fodselsnummerMaVare11Siffer:
+          "Social security number or D-number must be 11 digits",
+        fornavnPakrevd:
+          "First name is required and must be at least 2 characters",
+        etternavnPakrevd:
+          "Last name is required and must be at least 2 characters",
+        fodselsdatoPakrevd: "Date of birth is required",
+        maVelgeAktivitetNarIkkeLonnetArbeid:
+          "You must select an activity when you have not been in paid employment",
+        maSvarePaLonnetArbeid:
+          "You must answer whether you have been or will be in paid employment in Norway before assignment",
+        maSvarePaFlereVirksomheter:
+          "You must answer whether you will work for multiple companies during the period",
+      },
+      aktiviteter: {
+        studier: "Studies",
+        ferie: "Vacation",
+        selvstendingVirksomhet: "Self-employed business",
+        kontantytelseFraNav: "Received cash benefits from Nav",
+      },
+    },
+  },
+};
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,11 +1,25 @@
 import "./index.css";
 
+import { onLanguageSelect } from "@navikt/nav-dekoratoren-moduler";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
+import i18n from "i18next";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { initReactI18next } from "react-i18next";
 
+import { resources } from "./i18n/i18n.ts";
 import { routeTree } from "./routeTree.gen";
+
+// Initialize i18n
+i18n.use(initReactI18next).init({
+  lng: "nb",
+  fallbackLng: "nb",
+  resources,
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 export const queryClient = new QueryClient();
 
@@ -23,6 +37,10 @@ declare module "@tanstack/react-router" {
     router: typeof router;
   }
 }
+
+onLanguageSelect((language) => {
+  i18n.changeLanguage(language.locale);
+});
 
 createRoot(document.querySelector("#root")!).render(
   <StrictMode>
