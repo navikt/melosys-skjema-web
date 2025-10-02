9acde13cd40ee23dd00fdb5c10bb1cfc1430325e
Øyvind Nygard <oyvinyg@gmail.com>
2025-10-02 13:13:10 +0200

    Splittet ut valideringer i /arbeidsgiver til separate funksjoner (#37)

--- app/src/pages/skjema/arbeidsgiver/arbeidsgiverStegSchema.ts
+++ app/src/pages/skjema/arbeidsgiver/arbeidsgiverStegSchema.ts
@@ -1,8 +1,28 @@
 import { z } from "zod";
 
-export const arbeidsgiverSchema = z.object({
-  organisasjonsnummer: z
-    .string()
-    .min(1, "generellValidering.organisasjonsnummerErPakrevd")
-    .regex(/^\d{9}$/, "generellValidering.organisasjonsnummerMaVare9Siffer"),
+const baseArbeidsgiverSchema = z.object({
+  organisasjonsnummer: z.string(),
 });
+
+type BaseArbeidsgiverFormData = z.infer<typeof baseArbeidsgiverSchema>;
+
+function validerOrganisasjonsnummerPakrevd(data: BaseArbeidsgiverFormData) {
+  return data.organisasjonsnummer && data.organisasjonsnummer.length > 0;
+}
+
+function validerOrganisasjonsnummerFormat(data: BaseArbeidsgiverFormData) {
+  if (data.organisasjonsnummer && data.organisasjonsnummer.length > 0) {
+    return /^\d{9}$/.test(data.organisasjonsnummer);
+  }
+  return true;
+}
+
+export const arbeidsgiverSchema = baseArbeidsgiverSchema
+  .refine(validerOrganisasjonsnummerPakrevd, {
+    message: "generellValidering.organisasjonsnummerErPakrevd",
+    path: ["organisasjonsnummer"],
+  })
+  .refine(validerOrganisasjonsnummerFormat, {
+    message: "generellValidering.organisasjonsnummerMaVare9Siffer",
+    path: ["organisasjonsnummer"],
+  });
