13d963c464e23bfc883aad125c7642c035eded3c
oyvinyg <oyvinyg@gmail.com>
2025-10-13 12:16:31 +0200

    Oppretter arbeidstakers skjema ved "start søknad"

--- app/src/pages/skjema/arbeidstaker/ArbeidstakerSkjemaVeiledning.tsx
+++ app/src/pages/skjema/arbeidstaker/ArbeidstakerSkjemaVeiledning.tsx
@@ -1,13 +1,40 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useNavigate } from "@tanstack/react-router";
+import { toast } from "react-hot-toast";
 
+import { getUserInfo } from "~/httpClients/dekoratorenClient.ts";
+import { createArbeidstakerSkjema } from "~/httpClients/melsosysSkjemaApiClient.ts";
 import { ARBEIDSTAKER_STEG_REKKEFOLGE } from "~/pages/skjema/arbeidstaker/stegRekkefølge.ts";
 import { SkjemaVeiledning } from "~/pages/skjema/components/SkjemaVeiledning.tsx";
 
 export function ArbeidstakerSkjemaVeiledning() {
   const navigate = useNavigate();
-  const startRoute = ARBEIDSTAKER_STEG_REKKEFOLGE[0]?.route;
+  const { data: userInfo, isLoading, isError } = useQuery(getUserInfo());
 
-  return (
-    <SkjemaVeiledning onStartSoknad={() => navigate({ to: startRoute })} />
-  );
+  const createSkjemaMutation = useMutation({
+    mutationFn: createArbeidstakerSkjema,
+    onSuccess: (skjemaResponse) => {
+      const firstStepRoute = ARBEIDSTAKER_STEG_REKKEFOLGE[0]?.route.replace(
+        "$id",
+        skjemaResponse.id,
+      );
+      navigate({ to: firstStepRoute });
+    },
+    onError: () => {
+      toast.error("Kunne ikke opprette skjema. Prøv igjen.");
+    },
+  });
+
+  const handleStartSoknad = () => {
+    if (!userInfo?.userId) {
+      toast.error("Mangler brukerinformasjon");
+      return;
+    }
+    createSkjemaMutation.mutate({ fnr: userInfo.userId });
+  };
+
+  if (isLoading) return <div>Laster...</div>;
+  if (isError) return <div>Feil ved henting av brukerinformasjon</div>;
+
+  return <SkjemaVeiledning onStartSoknad={handleStartSoknad} />;
 }
