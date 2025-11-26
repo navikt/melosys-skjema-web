2eb77191781b661afdfdd0ebf6c10dcb8b84e2db
oyvinyg <oyvinyg@gmail.com>
2025-11-26 11:52:13 +0100

    Skrevet om til å ikke bruke useEffect i RadioGroupJaNeiFormPart.tsx

--- app/src/components/RadioGroupJaNeiFormPart.tsx
+++ app/src/components/RadioGroupJaNeiFormPart.tsx
@@ -1,5 +1,4 @@
 import { Radio, RadioGroup, RadioGroupProps } from "@navikt/ds-react";
-import { useEffect } from "react";
 import { Controller, useFormContext } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 
@@ -18,39 +17,45 @@ export function RadioGroupJaNeiFormPart({
   lockedValue,
   ...props
 }: RadioGroupJaNeiProps) {
-  const { control, setValue } = useFormContext();
+  const { control } = useFormContext();
   const { t } = useTranslation();
   const translateError = useTranslateError();
-
-  useEffect(() => {
-    if (lockedValue !== undefined) {
-      setValue(formFieldName, lockedValue);
-    }
-  }, [lockedValue, formFieldName, setValue]);
 
   return (
     <Controller
       control={control}
       name={formFieldName}
-      render={({ field, fieldState }) => (
-        <RadioGroup
-          {...props}
-          error={translateError(fieldState.error?.message)}
-          onChange={(value) => field.onChange(value === "true")}
-          value={
-            field.value === null || field.value === undefined
-              ? ""
-              : field.value.toString()
-          }
-        >
-          <Radio size="small" value="true">
-            {t("felles.ja")}
-          </Radio>
-          <Radio size="small" value="false">
-            {t("felles.nei")}
-          </Radio>
-        </RadioGroup>
-      )}
+      render={({ field, fieldState }) => {
+        // Når lockedValue er satt, bruk den i stedet for field.value
+        const effectiveValue =
+          lockedValue !== undefined ? lockedValue : field.value;
+
+        return (
+          <RadioGroup
+            {...props}
+            disabled={lockedValue !== undefined || props.disabled}
+            error={translateError(fieldState.error?.message)}
+            onChange={(value) => {
+              // Bare oppdater form state hvis ikke låst
+              if (lockedValue === undefined) {
+                field.onChange(value === "true");
+              }
+            }}
+            value={
+              effectiveValue === null || effectiveValue === undefined
+                ? ""
+                : effectiveValue.toString()
+            }
+          >
+            <Radio size="small" value="true">
+              {t("felles.ja")}
+            </Radio>
+            <Radio size="small" value="false">
+              {t("felles.nei")}
+            </Radio>
+          </RadioGroup>
+        );
+      }}
     />
   );
 }
