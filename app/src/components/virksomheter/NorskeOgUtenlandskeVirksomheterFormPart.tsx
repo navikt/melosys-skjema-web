24975f558eb1afe33bd1191a0d1cf60bf09e6b7a
Øyvind Nygard <oyvinyg@gmail.com>
2025-11-26 13:13:22 +0100

    NOJIRA Fjernet unødvendig bruk av useEffect (#124)
    * Skrevet om til å ikke bruke useEffect i ArbeidsgiverVelger.tsx
    
    * Skrevet om til å ikke bruke useEffect i DineOpplysningerSteg.tsx
    
    * Skrevet om til å ikke bruke useEffect i RadioGroupJaNeiFormPart.tsx
    
    * Skrevet om til å ikke bruke useEffect i NorskeOgUtenlandskeVirksomheterFormPart.tsx

--- app/src/components/virksomheter/NorskeOgUtenlandskeVirksomheterFormPart.tsx
+++ app/src/components/virksomheter/NorskeOgUtenlandskeVirksomheterFormPart.tsx
@@ -1,5 +1,4 @@
 import { Detail, ErrorMessage, Label, VStack } from "@navikt/ds-react";
-import { useEffect } from "react";
 import { useFormContext, useWatch } from "react-hook-form";
 
 import { NorskeVirksomheterFormPart } from "~/components/virksomheter/NorskeVirksomheterFormPart.tsx";
@@ -19,7 +18,6 @@ export function NorskeOgUtenlandskeVirksomheterFormPart({
 }: NorskeOgUtenlandskeVirksomheterFormPartProps) {
   const {
     formState: { errors },
-    clearErrors,
   } = useFormContext();
   const translateError = useTranslateError();
 
@@ -31,15 +29,14 @@ export function NorskeOgUtenlandskeVirksomheterFormPart({
     name: `${fieldName}.utenlandskeVirksomheter`,
   });
 
-  useEffect(() => {
-    const harVirksomheter =
-      (norskeVirksomheter && norskeVirksomheter.length > 0) ||
-      (utenlandskeVirksomheter && utenlandskeVirksomheter.length > 0);
+  // Beregn om det er virksomheter - hvis det er det, skal vi ikke vise erroren
+  // selv om den teknisk eksisterer i form state
+  const harVirksomheter =
+    (norskeVirksomheter && norskeVirksomheter.length > 0) ||
+    (utenlandskeVirksomheter && utenlandskeVirksomheter.length > 0);
 
-    if (harVirksomheter) {
-      clearErrors(fieldName);
-    }
-  }, [norskeVirksomheter, utenlandskeVirksomheter, fieldName, clearErrors]);
+  // Bare vis error hvis det faktisk ikke er noen virksomheter
+  const shouldShowError = error && !harVirksomheter;
   return (
     <VStack className="mt-4" gap="space-4">
       {label && <Label>{label}</Label>}
@@ -53,7 +50,7 @@ export function NorskeOgUtenlandskeVirksomheterFormPart({
         fieldName={`${fieldName}.utenlandskeVirksomheter`}
       />
 
-      {error && (
+      {shouldShowError && (
         <ErrorMessage className="mt-2">
           {translateError(error.message as string | undefined)}
         </ErrorMessage>
