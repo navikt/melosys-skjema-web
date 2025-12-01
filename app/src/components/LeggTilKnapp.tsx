a18e357488c3a42e3f18c58f423e38ef35ef2155
Øyvind Nygard <oyvinyg@gmail.com>
2025-12-01 08:47:42 +0100

    Oppdatert dependencies manuelt (#135)
    * Oppdatert dependencies manuelt
    
    * Oppdatert dependencies manuelt
    
    * lint fix

--- app/src/components/LeggTilKnapp.tsx
+++ app/src/components/LeggTilKnapp.tsx
@@ -2,8 +2,10 @@ import { PlusIcon } from "@navikt/aksel-icons";
 import { Button, ButtonProps } from "@navikt/ds-react";
 import { useTranslation } from "react-i18next";
 
-interface LeggTilKnappProps
-  extends Omit<ButtonProps, "icon" | "size" | "variant"> {
+interface LeggTilKnappProps extends Omit<
+  ButtonProps,
+  "icon" | "size" | "variant"
+> {
   children?: React.ReactNode;
 }
 
