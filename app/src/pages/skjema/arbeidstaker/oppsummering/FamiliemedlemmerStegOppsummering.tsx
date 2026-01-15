5e0fe0d7a58e4c7a7e40b4a9ac7113c23999f80e
oyvinyg <oyvinyg@gmail.com>
2026-01-15 14:12:05 +0100

    Lagt til oppsummering for familiemedlemmer

--- app/src/pages/skjema/arbeidstaker/oppsummering/FamiliemedlemmerStegOppsummering.tsx
+++ app/src/pages/skjema/arbeidstaker/oppsummering/FamiliemedlemmerStegOppsummering.tsx
@@ -1,11 +1,50 @@
 import { FormSummary } from "@navikt/ds-react";
 import { useTranslation } from "react-i18next";
 
+import { Familiemedlem } from "~/types/melosysSkjemaTypes.ts";
 import { useBooleanToJaNei } from "~/utils/translation.ts";
 
 import { stepKey as familiemedlemmerStepKey } from "../familiemedlemmer/FamiliemedlemmerSteg.tsx";
 import { ARBEIDSTAKER_STEG_REKKEFOLGE } from "../stegRekkefølge.ts";
 import { ArbeidstakerSkjemaProps } from "../types.ts";
+
+function FamiliemedlemOppsummeringItem({
+  familiemedlem,
+}: {
+  familiemedlem: Familiemedlem;
+}) {
+  const { t } = useTranslation();
+
+  const fields = [
+    {
+      label: t("familiemedlemmerSteg.fornavn"),
+      value: familiemedlem.fornavn,
+    },
+    {
+      label: t("familiemedlemmerSteg.etternavn"),
+      value: familiemedlem.etternavn,
+    },
+    {
+      label: familiemedlem.harNorskFodselsnummerEllerDnummer
+        ? t("familiemedlemmerSteg.fodselsnummer")
+        : t("familiemedlemmerSteg.fodselsdato"),
+      value: familiemedlem.harNorskFodselsnummerEllerDnummer
+        ? familiemedlem.fodselsnummer
+        : familiemedlem.fodselsdato,
+    },
+  ];
+
+  return (
+    <FormSummary.Answers>
+      {fields.map((field, index) => (
+        <FormSummary.Answer key={index}>
+          <FormSummary.Label>{field.label}</FormSummary.Label>
+          <FormSummary.Value>{field.value}</FormSummary.Value>
+        </FormSummary.Answer>
+      ))}
+    </FormSummary.Answers>
+  );
+}
 
 export function FamiliemedlemmerStegOppsummering({
   skjema,
@@ -37,6 +76,30 @@ export function FamiliemedlemmerStegOppsummering({
               {booleanToJaNei(familiemedlemmerData.skalHaMedFamiliemedlemmer)}
             </FormSummary.Value>
           </FormSummary.Answer>
+
+          {familiemedlemmerData.familiemedlemmer.length > 0 && (
+            <FormSummary.Answer>
+              <FormSummary.Label>
+                {t("familiemedlemmerSteg.familiemedlemmer")}
+              </FormSummary.Label>
+              <FormSummary.Value
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: "var(--a-spacing-2)",
+                }}
+              >
+                {familiemedlemmerData.familiemedlemmer.map(
+                  (familiemedlem, index) => (
+                    <FamiliemedlemOppsummeringItem
+                      familiemedlem={familiemedlem}
+                      key={index}
+                    />
+                  ),
+                )}
+              </FormSummary.Value>
+            </FormSummary.Answer>
+          )}
         </FormSummary.Answers>
 
         <FormSummary.Footer>
