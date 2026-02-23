fdbcf00279c0921397fd5a87483414ab0cf44e99
Øyvind Nygard <oyvinyg@gmail.com>
2026-02-23 10:06:43 +0100

    7882 Oppdater styling på AppHeader og KontekstVelger (#257)
    Oppdaterer styling på AppHeader og KontekstVelger med grå
    bakgrunn, truncation av lange navn og Label-komponent.
    
    Lange rådgiverfirma-navn kunne bryte layouten i KontekstVelger,
    og AppHeader manglet tydelig visuell avgrensning mot resten
    av siden.

--- app/src/routes/__root.tsx
+++ app/src/routes/__root.tsx
@@ -11,9 +11,15 @@ export const Route = createRootRouteWithContext<RouterContext>()({
 function RootComponent() {
   return (
     <Page footerPosition="belowFold">
+      <div
+        style={{ backgroundColor: "var(--ax-bg-neutral-soft)", width: "100%" }}
+      >
+        <Page.Block gutters style={{ paddingInline: "24px" }} width="md">
+          <AppHeader />
+        </Page.Block>
+      </div>
       <Page.Block gutters style={{ paddingInline: "24px" }} width="md">
         <VStack as="main" gap="space-32" paddingBlock="space-32 space-0">
-          <AppHeader />
           <Outlet />
         </VStack>
       </Page.Block>
