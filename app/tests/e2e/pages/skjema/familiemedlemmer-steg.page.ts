b2f8ab5a3cb8cdcebf335de7339f703b596d5919
Odin Eilertsen <odin.eilertsen@nav.no>
2026-05-13 14:04:00 +0200

    Fjern modal-kode fra familiemedlemmer page object

--- app/tests/e2e/pages/skjema/familiemedlemmer-steg.page.ts
+++ app/tests/e2e/pages/skjema/familiemedlemmer-steg.page.ts
@@ -12,25 +12,12 @@ import type { RadioButtonGroupJaNeiLocator } from "../../../types/playwright-typ
 // Hent felter fra statiske definisjoner
 const familiemedlemmerSeksjon = SKJEMA_DEFINISJON_A1.seksjoner.familiemedlemmer;
 const felter = familiemedlemmerSeksjon.felter;
-const elementDef = felter.familiemedlemmer.elementDefinisjon;
 const t = nb.translation;
 
-// Feilmeldinger - hoveddelen
+// Feilmeldinger
 const feilmeldinger = {
   duMaSvarePaOmDuHarFamiliemedlemmer:
     t.familiemedlemmerSteg.duMaSvarePaOmDuHarFamiliemedlemmerSomSkalVaereMed,
-};
-
-// Feilmeldinger - modal (familiemedlem)
-const modalFeilmeldinger = {
-  fornavnErPakrevd: t.familiemedlemmerSteg.fornavnErPakrevd,
-  etternavnErPakrevd: t.familiemedlemmerSteg.etternavnErPakrevd,
-  duMaSvarePaOmHarNorskFnr:
-    t.familiemedlemmerSteg
-      .duMaSvarePaOmFamiliemedlemmetHarNorskFodselsnummerEllerDnummer,
-  fodselsnummerErPakrevd: t.familiemedlemmerSteg.fodselsnummerErPakrevd,
-  ugyldigFodselsnummer: t.felles.ugyldigFodselsnummerEllerDnummer,
-  fodselsdatoErPakrevd: t.familiemedlemmerSteg.fodselsdatoErPakrevd,
 };
 
 export class FamiliemedlemmerStegPage {
@@ -38,18 +25,8 @@ export class FamiliemedlemmerStegPage {
   readonly skjema: UtsendtArbeidstakerSkjemaDto;
   readonly heading: Locator;
   readonly harDuFamiliemedlemmerSomSkalVaereMedRadioGroup: RadioButtonGroupJaNeiLocator;
-  readonly leggTilFamiliemedlemButton: Locator;
   readonly lagreOgFortsettButton: Locator;
   readonly infoboks: Locator;
-
-  // Modal locators (beholdt for fremtidig bruk)
-  readonly modal: Locator;
-  readonly modalFornavnInput: Locator;
-  readonly modalEtternavnInput: Locator;
-  readonly modalHarNorskFnrRadioGroup: RadioButtonGroupJaNeiLocator;
-  readonly modalFodselsnummerInput: Locator;
-  readonly modalFodselsdatoInput: Locator;
-  readonly modalLagreButton: Locator;
 
   constructor(page: Page, skjema: UtsendtArbeidstakerSkjemaDto) {
     this.page = page;
@@ -73,41 +50,8 @@ export class FamiliemedlemmerStegPage {
       }),
     };
 
-    this.leggTilFamiliemedlemButton = page.getByRole("button", {
-      name: felter.familiemedlemmer.leggTilLabel,
-    });
-
     this.infoboks = page.getByRole("region").filter({
       hasText: t.familiemedlemmerSteg.informasjonOmEgenSoknad,
-    });
-
-    // Modal locators (scoped to the dialog)
-    this.modal = page.getByRole("dialog");
-    this.modalFornavnInput = this.modal.getByLabel(elementDef.fornavn.label);
-    this.modalEtternavnInput = this.modal.getByLabel(
-      elementDef.etternavn.label,
-    );
-
-    const modalHarNorskFnrGroup = this.modal.getByRole("radiogroup", {
-      name: elementDef.harNorskFodselsnummerEllerDnummer.label,
-    });
-    this.modalHarNorskFnrRadioGroup = {
-      JA: modalHarNorskFnrGroup.getByRole("radio", {
-        name: nb.translation.felles.ja,
-      }),
-      NEI: modalHarNorskFnrGroup.getByRole("radio", {
-        name: nb.translation.felles.nei,
-      }),
-    };
-
-    this.modalFodselsnummerInput = this.modal.getByLabel(
-      elementDef.fodselsnummer.label,
-    );
-    this.modalFodselsdatoInput = this.modal.getByLabel(
-      elementDef.fodselsdato.label,
-    );
-    this.modalLagreButton = this.modal.getByRole("button", {
-      name: nb.translation.felles.lagre,
     });
 
     this.lagreOgFortsettButton = page.getByRole("button", {
@@ -167,7 +111,7 @@ export class FamiliemedlemmerStegPage {
     ).not.toBeVisible();
   }
 
-  // --- Validation assertions: hoveddelen ---
+  // --- Validation assertions ---
 
   private skalHaMedFamiliemedlemmerFieldset() {
     return this.page.getByRole("radiogroup", {
@@ -181,57 +125,5 @@ export class FamiliemedlemmerStegPage {
         feilmeldinger.duMaSvarePaOmDuHarFamiliemedlemmer,
       ),
     ).toBeVisible();
-  }
-
-  // --- Validation assertions: modal ---
-
-  /**
-   * Opens the "Legg til familiemedlem" modal and clicks Lagre without
-   * filling anything, leaving the modal open for validation assertions.
-   */
-  async clickLagreInFamiliemedlemModal() {
-    await this.leggTilFamiliemedlemButton.click();
-    await expect(this.modal).toBeVisible();
-    await this.modalLagreButton.click();
-  }
-
-  async assertModalFornavnErPakrevdIsVisible() {
-    await expect(
-      this.modal.getByText(modalFeilmeldinger.fornavnErPakrevd),
-    ).toBeVisible();
-  }
-
-  async assertModalEtternavnErPakrevdIsVisible() {
-    await expect(
-      this.modal.getByText(modalFeilmeldinger.etternavnErPakrevd),
-    ).toBeVisible();
-  }
-
-  async assertModalDuMaSvarePaOmHarNorskFnrIsVisible() {
-    await expect(
-      this.modal.getByText(modalFeilmeldinger.duMaSvarePaOmHarNorskFnr),
-    ).toBeVisible();
-  }
-
-  async assertModalFodselsnummerErPakrevdIsVisible() {
-    await expect(
-      this.modal.getByText(modalFeilmeldinger.fodselsnummerErPakrevd),
-    ).toBeVisible();
-  }
-
-  async assertModalUgyldigFodselsnummerIsVisible() {
-    await expect(
-      this.modal.getByText(modalFeilmeldinger.ugyldigFodselsnummer),
-    ).toBeVisible();
-  }
-
-  async assertModalFodselsdatoErPakrevdIsVisible() {
-    await expect(
-      this.modal.getByText(modalFeilmeldinger.fodselsdatoErPakrevd),
-    ).toBeVisible();
-  }
-
-  async assertFamiliemedlemModalIsOpen() {
-    await expect(this.modal).toBeVisible();
   }
 }
