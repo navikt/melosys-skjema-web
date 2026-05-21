bbec54bb2494b81ed45d9754e464ac3a96e8e7d7
Isa Salem Hame <isa.salem.hame@nav.no>
2026-05-21 13:52:33 +0200

    Sett opp base path i Vite-config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: process.env.BASE_PATH || "/",
   plugins: [react(), tanstackRouter(), tailwindcss()],
   resolve: {
     alias: {
